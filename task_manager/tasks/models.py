0cedb207fb08155b730bfb67a372337661c3ce01
bulldozer <loshka5647@gmail.com>
2023-11-11 17:16:51 +0200

    Add Tasks database

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Task(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+
+    L = "L"
+    M = "M"
+    H = "H"
+    PRIORITY_CHOICES = [
+        (L, "Low"),
+        (M, "Medium"),
+        (H, "High")
+    ]
+
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=L)
+
+    def __str__(self):
+        return self.title
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 def home(request):
-    return render(request, "tasks/home.html", context=None)
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        task.idhtml = task.id.hex[:8]
+
+    context = {
+        "tasks": tasks,
+    }
+
+    return render(request, "tasks/home.html", context=context)
